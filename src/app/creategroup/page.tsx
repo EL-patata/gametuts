da00d62dceb2c63770102b365be5f2bad7b53703
EL-patata <youssiftharwat26@gmail.com>
2023-11-16 23:49:45 +0200

    first commit for netlify .1

--- src/app/creategroup/page.tsx
+++ src/app/creategroup/page.tsx
@@ -1,10 +1,15 @@
-'use client';
 import Creategroup from '@/components/group/Creategroup';
+import { currentUser } from '@clerk/nextjs';
+import { redirect } from 'next/navigation';
 import { FC } from 'react';
 
 type Props = {};
 
-const page: FC<Props> = ({}) => {
+const page: FC<Props> = async ({}) => {
+	const user = await currentUser();
+
+	if (user) redirect(`/sign-in`);
+
 	return (
 		<main>
 			<Creategroup />
